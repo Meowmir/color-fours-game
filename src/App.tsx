e981c5398c7da98c546e5c9199b71d9062f3184f
Meowmir <disewji@gmail.com>
2023-08-21 15:03:39 +0200

    did it work

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 
 import "./App.css";
+import { BoardGrid } from "./functions";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        <BoardGrid></BoardGrid>
       </header>
     </div>
   );
