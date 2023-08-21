8ccaa9252d46b59bc5c0756b1f0152abe51ea4b7
Meowmir <disewji@gmail.com>
2023-08-21 21:47:26 +0200

    Hugo helped, fixed the game board, added functions, inception...

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 
 import "./App.css";
-import { BoardGrid } from "./functions";
+import { BoardGrid } from "./board-grid";
 
 function App() {
   return (
