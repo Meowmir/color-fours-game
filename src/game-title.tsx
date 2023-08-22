39b36f44b84c966537946efbe53c26f3edb42d67
Meowmir <disewji@gmail.com>
2023-08-22 10:17:07 +0200

    changed to two h1 in an empty fragment

--- src/game-title.tsx
+++ src/game-title.tsx
@@ -1,19 +1,20 @@
 export function GameTitle() {
   return (
-    <h1>
-      <text style={{ color: "#00D6F2" }}>C</text>
-      <text style={{ color: "#90EA00" }}>O</text>
-      <text style={{ color: "#FFB100" }}>L</text>
-      <text style={{ color: "#EA0090" }}>O</text>
-      <text style={{ color: "#00D6F2" }}>R</text>
-      <br />
-      <text style={{ color: "#90EA00" }}>F</text>
-      <text style={{ color: "#FFB100" }}>O</text>
-      <text style={{ color: "#EA0090" }}>U</text>
-      <text style={{ color: "#00D6F2" }}>R</text>
-      <text style={{ color: "#90EA00" }}>S</text>
-    </h1>
+    <>
+      <h1>
+        <text style={{ color: "#00D6F2" }}>C</text>
+        <text style={{ color: "#90EA00" }}>O</text>
+        <text style={{ color: "#FFB100" }}>L</text>
+        <text style={{ color: "#EA0090" }}>O</text>
+        <text style={{ color: "#00D6F2" }}>R</text>
+      </h1>
+      <h1>
+        <text style={{ color: "#90EA00" }}>F</text>
+        <text style={{ color: "#FFB100" }}>O</text>
+        <text style={{ color: "#EA0090" }}>U</text>
+        <text style={{ color: "#00D6F2" }}>R</text>
+        <text style={{ color: "#90EA00" }}>S</text>
+      </h1>
+    </>
   );
 }
-
-// Color <br /> Fours
