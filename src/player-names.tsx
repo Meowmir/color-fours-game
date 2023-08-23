00dd46a0d5ae715556f680fc1cc374f995d306f6
Meowmir <disewji@gmail.com>
2023-08-23 15:17:59 +0200

    first draft of textfield for players to enter names

---
+++ src/player-names.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { TextField } from "@mui/material";
+
+export function PlayerNames() {
+  return;
+  <>
+    <TextField
+      id="outlined-basic"
+      label="Player 1"
+      color="secondary"
+      variant="outlined"
+    />
+    <TextField
+      id="outlined-basic"
+      label="Player 1"
+      color="secondary"
+      variant="outlined"
+    />
+  </>;
+}
