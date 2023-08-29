b612feb83f8693ea1408649bb40a3782c6a7c93a
Meowmir <disewji@gmail.com>
2023-08-29 13:32:27 +0200

    fixed gameSocket recieving P1 name, added textfield

--- src/pages/new-game.tsx
+++ src/pages/new-game.tsx
@@ -1,10 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNewGame } from "../game-socket";
 import { useNavigate } from "react-router-dom";
+import { Button, TextField } from "@mui/material";
 
 export default function NewGame() {
   const navigate = useNavigate();
-  const [newGame, isLoading, createGame] = useNewGame();
+  const [newGame, player, isLoading, createGame] = useNewGame();
+  const [player1, setPlayer1] = useState("");
 
   useEffect(() => {
     if (!newGame) return;
@@ -12,5 +14,22 @@ export default function NewGame() {
     navigate(`/running-game/${newGame.gameId}`);
   }, [navigate, newGame]);
 
-  return <h2 onClick={createGame}>NEW GAME</h2>;
+  return (
+    <>
+      <br />
+      <TextField
+        id="outlined-basic"
+        label="Player 1"
+        variant="outlined"
+        value={player1}
+        onChange={(e) => {
+          setPlayer1(e.target.value);
+        }}
+      />
+      <br />
+      <Button variant="contained" onClick={() => createGame(player1)}>
+        NEW GAME
+      </Button>
+    </>
+  );
 }
