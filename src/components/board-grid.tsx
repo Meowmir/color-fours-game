b89520d43da03150120c3f8188c1ce5943f050c9
Meowmir <disewji@gmail.com>
2023-09-08 09:48:00 +0200

    small change to get player names

--- src/components/board-grid.tsx
+++ src/components/board-grid.tsx
@@ -1,10 +1,12 @@
+import React, { FC } from "react";
+import { useParams } from "react-router-dom";
 import { Box, Container } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
-import React, { FC } from "react";
+
 import { PlayerChip } from "./player-chips";
 import { BoardTile } from "./board-tile";
-
 import { Game, ReadTileDTO } from "../my-types";
+import { useRunningGame } from "../hooks/use-running-game.hook";
 
 const COL_COUNT = 12;
 
@@ -13,6 +15,19 @@ export interface BoardProps {
 }
 
 export const BoardGrid: FC<BoardProps> = ({ game }) => {
+  const { gameId } = useParams();
+  const [theGame, isLoading] = useRunningGame(gameId!);
+
+  if (!theGame) {
+    return <p>"Loading"</p>;
+  }
+
+  const { players } = theGame;
+  const [{ name: player1Name }, { name: player2Name }] = players;
+
+  console.log(player1Name);
+  console.log(player2Name);
+
   return (
     <Box>
       <Container>
