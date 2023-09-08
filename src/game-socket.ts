45f37ae82eb97276f9199ebc273ffe9ed238f0fa
Meowmir <disewji@gmail.com>
2023-09-08 15:18:53 +0200

    able to add player 2, player 1 sees loading screen

--- src/game-socket.ts
+++ src/game-socket.ts
@@ -2,10 +2,11 @@ import { io } from "socket.io-client";
 import { createContext } from "react";
 
 // http://localhost:3001
-const SOCKET_URL = "http://localhost:3001";
-//const SOCKET_URL = "http://64.225.83.55";
+//const SOCKET_URL = "http://localhost:3001";
+const SOCKET_URL = "http://64.225.83.55";
 const API_KEY_KEY = "myToken";
-const token = localStorage.getItem(API_KEY_KEY);
+const token =
+  "ZXlKaGJHY2lPaUpJVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5Sm9iM04wSWpvaWJHOWpZV3hvYjNOME9qTXdNREFpTENKcFlYUWlPakUyT1RJMU5UUTRNRE45LlVkU2JtSndyd2libnljS1lSMUxTaEJWU0w2VmVsQVQxTW41YUVZWDVZWmM="; //localStorage.getItem(API_KEY_KEY);
 
 export const gameSocket = io(SOCKET_URL, {
   auth: {
--- src/views/running-game.view.tsx
+++ src/views/running-game.view.tsx
@@ -1,17 +1,33 @@
 import React, { useCallback, useState } from "react";
 import { useParams } from "react-router-dom";
 
-import { Alert, Button, Snackbar } from "@mui/material";
+import { Button, Modal, Box, Stack } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 import { BoardGrid } from "../components/board-grid";
 import { useRunningGame } from "../hooks/use-running-game.hook";
 import { PlayerName } from "../components/player-names";
 import { useAddPlayer } from "../hooks/use-add-player.hook";
+import Grid from "@mui/material/Unstable_Grid2";
+import { PlayerChip } from "../components/player-chips";
+import { GameTitle } from "../components/game-title";
 
 const StyledDiv = styled("div")({
   marginTop: 30,
 });
+
+const styledBox = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  backgroundColor: "background.paper",
+  border: "2px solid #000",
+  borderRadius: 4,
+  boxShadow: 24,
+  padding: 4,
+};
 
 export default function RunningGameView() {
   const { gameId } = useParams();
@@ -49,8 +65,27 @@ export default function RunningGameView() {
   if (theGame.players.length < 2 && !isP1) {
     return (
       <StyledDiv>
+        <Grid container spacing={0}>
+          <Grid xs={2}>
+            <PlayerChip readOnly color="blue" />
+            <PlayerChip readOnly color="green" />
+            <PlayerChip readOnly color="orange" />
+            <PlayerChip readOnly color="pink" />
+          </Grid>
+          <Grid xs={8}>
+            <GameTitle />
+          </Grid>
+          <Grid xs={2}>
+            <PlayerChip readOnly fill color="blue" />
+            <PlayerChip readOnly fill color="green" />
+            <PlayerChip readOnly fill color="orange" />
+            <PlayerChip readOnly fill color="pink" />
+          </Grid>
+        </Grid>
+
         <PlayerName color="warning" label="Player 2" onChange={setPlayer2} />
-        <Button variant="contained" onClick={handleClick}>
+        <br />
+        <Button variant="contained" color="warning" onClick={handleClick}>
           ADD PLAYER 2
         </Button>
       </StyledDiv>
@@ -59,27 +94,25 @@ export default function RunningGameView() {
 
   return (
     <StyledDiv>
-      <PlayerName color="warning" label="Player 2" onChange={setPlayer2} />
-
-      <Snackbar
-        open={openAlertEmptyName}
-        onClose={() => setOpenAlertEmptyName(false)}
-        anchorOrigin={{ vertical: "top", horizontal: "center" }}
-      >
-        <Alert severity="error" sx={{ width: "100%" }}>
-          Player name can't be empty.
-        </Alert>
-      </Snackbar>
-      <Snackbar
-        open={openAlertShortName}
-        onClose={() => setOpenAlertShortName(false)}
-        anchorOrigin={{ vertical: "top", horizontal: "center" }}
-      >
-        <Alert severity="error" sx={{ width: "100%" }}>
-          Player name can't be less than 3 characters.
-        </Alert>
-      </Snackbar>
-      <BoardGrid game={theGame} />
+      <Grid container spacing={0}>
+        <Grid xs={2}>
+          <PlayerChip readOnly color="blue" />
+          <PlayerChip readOnly color="green" />
+          <PlayerChip readOnly color="orange" />
+          <PlayerChip readOnly color="pink" />
+        </Grid>
+        <Grid xs={8}>
+          <Box>
+            <img alt="" style={{ width: "50%" }} src="/circles@2x.png" />
+          </Box>
+        </Grid>
+        <Grid xs={2}>
+          <PlayerChip readOnly fill color="blue" />
+          <PlayerChip readOnly fill color="green" />
+          <PlayerChip readOnly fill color="orange" />
+          <PlayerChip readOnly fill color="pink" />
+        </Grid>
+      </Grid>
     </StyledDiv>
   );
 }
