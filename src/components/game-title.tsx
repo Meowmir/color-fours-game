ecc150a2f41479d9a1c194a53ca9cc61fe0c8826
Meowmir <disewji@gmail.com>
2023-09-13 22:13:37 +0200

    added a smaller version of the title

--- src/components/game-title.tsx
+++ src/components/game-title.tsx
@@ -18,3 +18,23 @@ export function GameTitle() {
     </>
   );
 }
+
+export function SmallGameTitle() {
+  return (
+    <>
+      <h2>
+        <span style={{ color: "#00D6F2" }}>C</span>
+        <span style={{ color: "#90EA00" }}>O</span>
+        <span style={{ color: "#FFB100" }}>L</span>
+        <span style={{ color: "#EA0090" }}>O</span>
+        <span style={{ color: "#00D6F2" }}>R</span>
+        <span> </span>
+        <span style={{ color: "#90EA00" }}>F</span>
+        <span style={{ color: "#FFB100" }}>O</span>
+        <span style={{ color: "#EA0090" }}>U</span>
+        <span style={{ color: "#00D6F2" }}>R</span>
+        <span style={{ color: "#90EA00" }}>S</span>
+      </h2>
+    </>
+  );
+}
