260d9416e43ec2054ec6786cecdb557ba226a3c0
Pugsie <disewji@gmail.com>
2023-10-22 20:44:57 +0200

    made only title clickable, not full width of page

--- src/components/displays/game-title.tsx
+++ src/components/displays/game-title.tsx
@@ -1,21 +1,21 @@
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 export function GameTitle() {
   return (
     <>
       <h1>
-        <span style={{ color: "#00D6F2" }}>C</span>
-        <span style={{ color: "#90EA00" }}>O</span>
-        <span style={{ color: "#FFB100" }}>L</span>
-        <span style={{ color: "#EA0090" }}>O</span>
-        <span style={{ color: "#00D6F2" }}>R</span>
+        <span style={{color: "#00D6F2"}}>C</span>
+        <span style={{color: "#90EA00"}}>O</span>
+        <span style={{color: "#FFB100"}}>L</span>
+        <span style={{color: "#EA0090"}}>O</span>
+        <span style={{color: "#00D6F2"}}>R</span>
       </h1>
       <h1>
-        <span style={{ color: "#90EA00" }}>F</span>
-        <span style={{ color: "#FFB100" }}>O</span>
-        <span style={{ color: "#EA0090" }}>U</span>
-        <span style={{ color: "#00D6F2" }}>R</span>
-        <span style={{ color: "#90EA00" }}>S</span>
+        <span style={{color: "#90EA00"}}>F</span>
+        <span style={{color: "#FFB100"}}>O</span>
+        <span style={{color: "#EA0090"}}>U</span>
+        <span style={{color: "#00D6F2"}}>R</span>
+        <span style={{color: "#90EA00"}}>S</span>
       </h1>
     </>
   );
@@ -26,18 +26,20 @@ export function SmallGameTitle() {
 
   return (
     <>
-      <h2 style={{ cursor: "pointer" }} onClick={() => navigate("/")}>
-        <span style={{ color: "#00D6F2" }}>C</span>
-        <span style={{ color: "#90EA00" }}>O</span>
-        <span style={{ color: "#FFB100" }}>L</span>
-        <span style={{ color: "#EA0090" }}>O</span>
-        <span style={{ color: "#00D6F2" }}>R</span>
-        <span> </span>
-        <span style={{ color: "#90EA00" }}>F</span>
-        <span style={{ color: "#FFB100" }}>O</span>
-        <span style={{ color: "#EA0090" }}>U</span>
-        <span style={{ color: "#00D6F2" }}>R</span>
-        <span style={{ color: "#90EA00" }}>S</span>
+      <h2>
+        <span style={{cursor: "pointer"}} onClick={() => navigate("/")}>
+          <span style={{color: "#00D6F2"}}>C</span>
+          <span style={{color: "#90EA00"}}>O</span>
+          <span style={{color: "#FFB100"}}>L</span>
+          <span style={{color: "#EA0090"}}>O</span>
+          <span style={{color: "#00D6F2"}}>R</span>
+          <span> </span>
+          <span style={{color: "#90EA00"}}>F</span>
+          <span style={{color: "#FFB100"}}>O</span>
+          <span style={{color: "#EA0090"}}>U</span>
+          <span style={{color: "#00D6F2"}}>R</span>
+          <span style={{color: "#90EA00"}}>S</span>
+        </span>
       </h2>
     </>
   );
